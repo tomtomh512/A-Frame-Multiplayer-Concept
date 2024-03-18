80fb8c1eba779041aec9a911be34330d282028b3
thoma <thomashsu0512@gmail.com>
2024-03-18 11:53:48 -0400

    beta bullet mech, health issue not fixed

--- script.js
+++ script.js
@@ -11,6 +11,15 @@ function cosInDegrees(angleInDegrees) {
     const angleInRadians = angleInDegrees * Math.PI / 180;
     return Math.cos(angleInRadians);
 }
+function atanInDegrees(y, x) {
+    // Convert from degrees to radians
+    const angleInRadians = Math.atan(y / x);
+
+    // Convert from radians to degrees
+    const angleInDegrees = angleInRadians * 180 / Math.PI;
+
+    return angleInDegrees;
+}
 
 window.onload = function() {
     let playerId; // string of who we are logged in as
@@ -26,16 +35,20 @@ window.onload = function() {
     const playerNameInput = document.querySelector("#player-name");
 
     function handleArrowPress() {
-        players[playerId].position.x = rig.getAttribute("position").x;
-        players[playerId].position.y = rig.getAttribute("position").y;
-        players[playerId].position.z = rig.getAttribute("position").z;
-        playerRef.set(players[playerId]); // causes value listener to fire
+        if (players[playerId] !== undefined) {
+            players[playerId].position.x = rig.getAttribute("position").x;
+            players[playerId].position.y = rig.getAttribute("position").y;
+            players[playerId].position.z = rig.getAttribute("position").z;
+            playerRef.set(players[playerId]);
+        }
     }
     function handleMouseMove() {
-        players[playerId].rotation.x = rig.getAttribute("rotation").x;
-        players[playerId].rotation.y = rig.getAttribute("rotation").y;
-        players[playerId].rotation.z = rig.getAttribute("rotation").z;
-        playerRef.set(players[playerId]);
+        if (players[playerId] !== undefined) {
+            players[playerId].rotation.x = rig.getAttribute("rotation").x;
+            players[playerId].rotation.y = rig.getAttribute("rotation").y;
+            players[playerId].rotation.z = rig.getAttribute("rotation").z;
+            playerRef.set(players[playerId]);
+        }
     }
 
     let count = 0; // ain't no way this going over 9007199254740991
@@ -66,50 +79,75 @@ window.onload = function() {
 
     function moveBullet(projectileKey) {
         let currentProjectile = projectiles[projectileKey];
-        let id = currentProjectile.id;
-        let projectileRef = firebase.database().ref(`projectiles/${id}`);
+        if (currentProjectile !== undefined) {
+            let id = currentProjectile.id;
+            let projectileRef = firebase.database().ref(`projectiles/${id}`);
 
-        // velocity
-        let magnitude = 15;
-        let dx = sinInDegrees(currentProjectile.rotation.y) / magnitude;
-        let dy = sinInDegrees(currentProjectile.rotation.x) / magnitude;
-        let dz = cosInDegrees(currentProjectile.rotation.y) / magnitude;
+            // velocity
+            let dx = sinInDegrees(currentProjectile.rotation.y);
+            let dy = sinInDegrees(currentProjectile.rotation.x);
+            let dz = cosInDegrees(currentProjectile.rotation.y);
 
-        // move, direct set
-        currentProjectile.position.x -= dx;
-        currentProjectile.position.y += dy;
-        currentProjectile.position.z -= dz;
-        projectileRef.set(projectiles[id]);
+            // move, direct set
+            let magnitude = 100; //more is slower, laggier
+            let timeout = 30;
+            currentProjectile.position.x -= dx / magnitude;
+            currentProjectile.position.y += dy / magnitude;
+            currentProjectile.position.z -= dz / magnitude;
+            //
+            // currentProjectile.position.x = currentProjectile.position.x - (dx / magnitude);
+            // currentProjectile.position.y = currentProjectile.position.y + (dy / magnitude);
+            // currentProjectile.position.z = currentProjectile.position.z - (dz / magnitude);
+            projectileRef.set(projectiles[id]);
 
-        // out of bounds and collisions
-        let range = 4;
-        if (Math.abs(currentProjectile.position.x) >= range || Math.abs(currentProjectile.position.y) >= range || Math.abs(currentProjectile.position.z) >= range) {
-            firebase.database().ref(`projectiles/${id}`).remove();
-        }
-
-        let projectileX = currentProjectile.position.x;
-        let projectileY = currentProjectile.position.y;
-        let projectileZ = currentProjectile.position.z;
-
-        for (let playerKey in players) {
-            let currentPlayer = players[playerKey];
-            let currentPlayerId = currentPlayer.id;
-            let playerRef = firebase.database().ref(`players/${currentPlayerId}`);
-            let playerX = currentPlayer.position.x;
-            let playerY = currentPlayer.position.y;
-            let playerZ = currentPlayer.position.z;
-
-            if (calculateDistance(projectileX, projectileY, projectileZ, playerX, playerY, playerZ) < 0.3 && currentProjectile.from !== currentPlayerId) {
+            // out of bounds and collisions
+            let range = 4;
+            if (
+                Math.abs(currentProjectile.position.x) >= range ||
+                Math.abs(currentProjectile.position.y) >= range ||
+                Math.abs(currentProjectile.position.z) >= range
+            ) {
                 firebase.database().ref(`projectiles/${id}`).remove();
-                playerRef.update({
-                    health: currentPlayer.health - 1,
-                })
+                // console.log("out of bounds");
             }
 
-            setTimeout(function () {
-                moveBullet(projectileKey);
-            }, 30);
+            let projectileX = currentProjectile.position.x;
+            let projectileY = currentProjectile.position.y;
+            let projectileZ = currentProjectile.position.z;
+
+            for (let playerKey in players) {
+                let currentPlayer = players[playerKey];
+                let currentPlayerId = currentPlayer.id;
+                let currentplayerRef = firebase.database().ref(`players/${currentPlayerId}`);
+                let playerX = currentPlayer.position.x;
+                let playerY = currentPlayer.position.y;
+                let playerZ = currentPlayer.position.z;
+
+                //let infoTagRef = firebase.database().ref(`infoTags/${currentPlayerId}`);
+
+                if (calculateDistance(projectileX, projectileY, projectileZ, playerX, playerY, playerZ) < 0.3 && currentProjectile.from !== currentPlayerId) {
+                    //gotHit(currentPlayerId);
+                    firebase.database().ref(`projectiles/${id}`).remove();
+                    gotHit(currentPlayerId);
+                }
+
+                setTimeout(function () {
+                    moveBullet(id);
+                }, timeout);
+            }
         }
+    }
+
+    function gotHit(key){
+        //let hitPlayer = players[key];
+        //let hitPlayerId = hitPlayer.id;
+        //let hitPlayerRef = firebase.database().ref(`players/${players[key].id}`);
+
+        // console.log(players[key].health + ", " + (players[key].health - 1));
+
+
+        players[key].health = players[key].health - 1;
+        //firebase.database().ref(`players/${players[key].id}`).set(players[players[key].id]);
     }
 
     function initGame() {
@@ -130,6 +168,9 @@ window.onload = function() {
         const allProjectilesRef = firebase.database().ref(`projectiles`);
 
         // when changes are made
+        // when new node is added
+        // remove when disconnect
+
         allPlayersRef.on("value", (snapshot) => {
             players = snapshot.val() || {};
             for (const key in players) {
@@ -148,7 +189,6 @@ window.onload = function() {
                 });
             }
         })
-        // when new node is added
         allPlayersRef.on("child_added", (snapshot) => {
             const addedPlayer = snapshot.val();
 
@@ -158,32 +198,31 @@ window.onload = function() {
             characterModel.setAttribute("depth", 0.33);
             characterModel.setAttribute("color","white");
             characterModel.setAttribute("position",{
-                x: addedPlayer.positionX,
-                y: addedPlayer.positionY,
-                z: addedPlayer.positionZ,
+                x: addedPlayer.position.x,
+                y: addedPlayer.position.y,
+                z: addedPlayer.position.z,
             });
             characterModel.setAttribute("rotation",{
-                x: addedPlayer.rotationX,
-                y: addedPlayer.rotationY,
-                z: addedPlayer.rotationZ,
+                x: addedPlayer.rotation.x,
+                y: addedPlayer.rotation.y,
+                z: addedPlayer.rotation.z,
             });
 
-                let smile = document.createElement("a-box");
-                smile.setAttribute("src", "img.png");
-                smile.setAttribute("height", 0.295);
-                smile.setAttribute("width", 0.295);
-                smile.setAttribute("depth", 0.295);
-                smile.setAttribute("position",{
-                    x: 0,
-                    y: 0,
-                    z: -0.02,
-                });
-                characterModel.append(smile);
+                // let smile = document.createElement("a-box");
+                // smile.setAttribute("src", "img.png");
+                // smile.setAttribute("height", 0.295);
+                // smile.setAttribute("width", 0.295);
+                // smile.setAttribute("depth", 0.295);
+                // smile.setAttribute("position",{
+                //     x: 0,
+                //     y: 0,
+                //     z: -0.02,
+                // });
+                // characterModel.append(smile);
 
             playerElements[addedPlayer.id] = characterModel;
             scene.appendChild(characterModel);
         })
-        // remove when disconnect
         allPlayersRef.on("child_removed", (snapshot) => {
             const id = snapshot.val().id;
             scene.remove(playerElements[id]);
@@ -209,6 +248,11 @@ window.onload = function() {
             const projectileModel = document.createElement("a-sphere");
             projectileModel.setAttribute("radius", 0.05);
             projectileModel.setAttribute("color", "red");
+            projectileModel.setAttribute("position",{
+                x: addedProjectile.position.x,
+                y: addedProjectile.position.y,
+                z: addedProjectile.position.z,
+            });
 
             projectileElements[addedProjectile.id] = projectileModel;
             scene.appendChild(projectileModel);
@@ -218,17 +262,6 @@ window.onload = function() {
             scene.remove(projectileElements[id]);
             delete projectileElements[id];
         })
-
-        // name tag change
-        playerNameInput.addEventListener("change", (e) => {
-            const newName = e.target.value;
-            playerNameInput.value = newName;
-            // only update keys given
-            playerRef.update({
-                name: newName,
-            });
-        })
-
     }
 
     firebase.auth().onAuthStateChanged((user) => {
@@ -241,7 +274,6 @@ window.onload = function() {
                 y: rig.getAttribute("position").y,
                 z: rig.getAttribute("position").z,
             }
-
             let rotation = {
                 x: rig.getAttribute("rotation").x,
                 y: rig.getAttribute("rotation").y,
@@ -251,7 +283,7 @@ window.onload = function() {
             playerRef.set({
                 id: playerId,
                 name: playerNameInput.value,
-                health: 100,
+                health: 10,
                 position,
                 rotation,
             })
